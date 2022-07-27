fc515c0b9cc5a1cdcd3ecca72b8fffee01dfa61a
RichardIrala <richardirala47@hotmail.com>
2022-07-26 21:16:16 -0300

    index creado. Posee: chequeo de conexión con la db e inicia el escuchador de express

---
+++ backend/index.ts
@@ -0,0 +1,15 @@
+import { sequelize } from "./database";
+import { app } from "./router/endpoints";
+
+(async function () {
+  const port = process.env.PORT || 3000;
+  try {
+    await sequelize.authenticate();
+    console.log("Autenticación completada");
+    app.listen(port, () => {
+      console.log("escuchando en el puerto 3000");
+    });
+  } catch (error) {
+    throw "Error al autenticar";
+  }
+})();
