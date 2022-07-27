38afaf10ebf31a76397dcd2d7001e105c8f05c9a
RichardIrala <richardirala47@hotmail.com>
2022-07-27 20:32:03 -0300

    Se agrego un nuevo parámetro al componente llamado extraClass. Permite agregar una clase además de la predefinida en el input

--- frontend/funcional-components/input.ts
+++ frontend/funcional-components/input.ts
@@ -1,14 +1,15 @@
 export const crearInput = (
   labelTitle: string,
   inputName: string,
-  inputType?: string
+  inputType?: string,
+  extraClass?: string
 ) => {
   return /*html*/ `
       <label class="label">
-        <span>${labelTitle}</span>
-        <input class="form__input" name=${inputName} type=${
-    inputType || "text"
-  }> 
+        <span>${labelTitle.toUpperCase()}</span>
+        <input class="form__input ${
+          extraClass ? extraClass : ""
+        }" name=${inputName} type=${inputType || "text"}> 
       </label>
     `;
 };
