f8b1bc46783c16cf9c86d4828353769cfd55b289
RichardIrala <richardirala47@hotmail.com>
2022-08-09 09:22:44 -0300

    Se agrego authMiddleware para chequear la autenticidad de los tokens que pasan por un endpoint

---
+++ backend/middlewares/authMiddleware.ts
@@ -0,0 +1,16 @@
+import "dotenv/config";
+import * as jwt from "jsonwebtoken";
+const SECRET = process.env.JWT_SECRET;
+//MIDDLEWARE QUE CHEQUEA EL JWT
+export const authMiddleware = async (req, res, next) => {
+  const headerAuthorization = req.get("Authorization");
+  const infoSpliteada = headerAuthorization.split(" ");
+  try {
+    const myInfoVerified = jwt.verify(infoSpliteada[1], SECRET);
+    const id = myInfoVerified.id;
+    req._userId = id;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: error.message });
+  }
+};
