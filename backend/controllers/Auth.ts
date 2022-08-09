14ebebf9bc4020370ea2ccfaba8f126fc24a6af9
RichardIrala <richardirala47@hotmail.com>
2022-08-09 09:27:27 -0300

    Se movio la constante SECRET arriba de todo. Y se modifico un comentario en la const auth

--- backend/controllers/Auth.ts
+++ backend/controllers/Auth.ts
@@ -1,16 +1,16 @@
 import * as crypto from "crypto";
 import * as jwt from "jsonwebtoken";
-import { Auth, User } from "../models";
+import { Auth, User } from "../models/index";
 import "dotenv/config";
+
+const SECRET = process.env.JWT_SECRET;
+
 //crypto function para hashear la password que vamos a ingresar en Auth
 function hashPassword(password: string) {
   return crypto.createHash("sha256").update(password).digest("hex");
 }
 
-/* Utilizo estos controllers modificados que utilice en algun momento. Falta quitarle dependencias que no le corresponden y transformarlo en una función independiente de un EndPoint para que sea más escalable a futuro */
-
-const SECRET = process.env.JWT_SECRET;
-
+//Crear usuarios y su autenticación evitando replicas.
 export const auth = async (
   email: string,
   firstName: string,
@@ -26,11 +26,10 @@ export const auth = async (
 
     const userId = usuario.get("id");
     const [auth, authCreated] = await Auth.findOrCreate({
-      where: { user_id: userId },
+      where: { UserId: userId },
       defaults: {
         email,
         password: passwordEncriptada,
-        user_id: userId,
       },
     });
 
@@ -40,8 +39,8 @@ export const auth = async (
   }
 };
 
-export const token = async (req, res) => {
-  const { password, email } = req.body;
+//Esto me devuelve mi token (id encryptado con revision de autenticidad)                                                                 <= ACTUAL TRABAJO en revision
+export const token = async (password: string, email: string) => {
   try {
     const passwordHasheada = hashPassword(password);
     const auth = await Auth.findOne({
@@ -49,29 +48,15 @@ export const token = async (req, res) => {
     });
 
     if (auth) {
-      const token = jwt.sign({ id: auth.get("user_id") }, SECRET);
-      res.json({ token });
-    } else res.status(400).json({ message: "Email o contraseña incorrecta" });
+      const token = jwt.sign({ id: auth.get("UserId") }, SECRET);
+      return { token };
+    } else return { message: "Email o contraseña incorrecta" };
   } catch (error) {
-    res.json({ message: error.message });
+    return { message: error.message };
   }
 };
 
 export const getMe = async (id: number) => {
   const myUserInfo = await User.findByPk(id);
   return myUserInfo;
-};
-
-//MIDDLEWARE QUE CHEQUEA EL JWT
-export const authMiddleware = async (req, res, next) => {
-  const headerAuthorization = req.get("Authorization");
-  const infoSpliteada = headerAuthorization.split(" ");
-  try {
-    const myInfoVerified = jwt.verify(infoSpliteada[1], SECRET);
-    const id = myInfoVerified.id;
-    req._userId = id;
-    next();
-  } catch (error) {
-    res.status(401).json({ message: error.message });
-  }
 };
