97bf3853b8f35aa182c3216a548914fe1cd448b0
RichardIrala <richardirala47@hotmail.com>
2022-08-09 09:25:04 -0300

    Se elimino el controller createUser

--- backend/controllers/User.ts
+++ backend/controllers/User.ts
@@ -1,15 +1,7 @@
 import { User } from "../models";
 
-export const createUser = async (email: string, firstName: string) => {
-  const [user, created] = await User.findOrCreate({
-    where: { email },
-    defaults: { firstName },
-  });
-  return user;
-};
-
 export const userExist = async (email: string) => {
   const user = await User.findOne({ where: { email } });
-  const exist = user != null;
+  const exist = user ? true : false;
   return { exist };
 };
