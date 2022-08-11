134a508fd560a053a0f66e7d12fa92466eca7229
RichardIrala <richardirala47@hotmail.com>
2022-08-11 00:54:44 -0300

    Se modifico la page agregandole los inputs que requería

--- frontend/pages/my-profile.ts
+++ frontend/pages/my-profile.ts
@@ -1,3 +1,6 @@
+import { crearInput, inputCss } from "../funcional-components/input";
+import { state } from "../state";
+
 export const instanciar_login_email_page = () => {
   customElements.define(
     "my-profile-el",
@@ -12,11 +15,26 @@ export const instanciar_login_email_page = () => {
         this.shadow.innerHTML = `
               <header-el></header-el>
               <div class="title-container">
-                  <title-el>Mascotas perdidas cerca tuyo</title-el>
+                  <title-el>Mis datos</title-el>
               </div>
-              <div class="pet-cards">
-                  HOLAAA
-              </div>
+              <form class="form d-flex flex-dir-column align-i-center gap20">
+                    ${crearInput(
+                      "nombre",
+                      "name",
+                      "text",
+                      "margin-b50",
+                      state.getUserName()
+                    )}
+                    ${crearInput("contraseña", "password", "password")}
+                    ${crearInput(
+                      "repetir contraseña",
+                      "repeated_password",
+                      "password"
+                    )}
+                    <button class="form__button" type="submit">
+                      <button-rose-el>Registrarse</button-rose-el>
+                    </button>
+              </form>
           `;
         style.innerHTML = `
           .title-container {
@@ -25,12 +43,41 @@ export const instanciar_login_email_page = () => {
               padding: 33px 0px;
           }
   
-          .pet-cards {
-              display: flex;
-              gap: 20px;
-              flex-direction: column;
-              align-items: center;
+          .d-flex {
+            display: flex;
           }
+
+          .flex-dir-column {
+            flex-direction: column;
+          }
+          
+          .gap20 {
+            gap: 20px;
+          }
+
+          .justify-c-center {
+            justify-content: center;
+          }
+
+          .align-i-center {
+            align-items: center;
+          }
+
+          .title-container {
+              display: flex;
+              justify-content: center;
+              padding: 33px 0px;
+          }
+
+          .form__button {
+            padding: 0;
+            border: none;
+          }
+
+          .margin-b50 {
+              margin-bottom: 50px;
+          }
+          ${inputCss()}
           `;
         this.shadow.appendChild(style);
         this.addListeners();
