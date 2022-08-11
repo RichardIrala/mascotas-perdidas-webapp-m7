00eddd3b1f0b4777ac464e412d866fdb88cc9740
RichardIrala <richardirala47@hotmail.com>
2022-08-11 00:53:58 -0300

    Se agrego un nuevo parámetro y acción en el componente crearInput

--- frontend/funcional-components/input.ts
+++ frontend/funcional-components/input.ts
@@ -2,14 +2,16 @@ export const crearInput = (
   labelTitle: string,
   inputName: string,
   inputType?: string,
-  extraClass?: string
+  extraClass?: string,
+  placeholder?: string
 ) => {
   return /*html*/ `
       <label class="label">
         <span>${labelTitle.toUpperCase()}</span>
-        <input class="form__input ${
-          extraClass ? extraClass : ""
-        }" name=${inputName} type=${inputType || "text"}> 
+        <input class="form__input ${extraClass ? extraClass : ""}" 
+        name=${inputName} 
+        type=${inputType || "text"}
+        placeholder=${placeholder || ""}> 
       </label>
     `;
 };
