e7fc4f406fd1c07c22cc5f255a493f339c792b5c
RichardIrala <richardirala47@hotmail.com>
2022-08-17 20:08:01 -0300

    Se creo una nueva page pets-reported.ts
    Esta página va a renderizar las mascotas reportadas por el usuario logueado

---
+++ frontend/pages/pets-reported.ts
@@ -0,0 +1,74 @@
+import { state } from "../state";
+import { api } from "../utils/api";
+
+export const instanciar_pets_reported_page = () => {
+  customElements.define(
+    "pets-reported-el",
+    class extends HTMLElement {
+      shadow = this.attachShadow({ mode: "open" });
+      constructor() {
+        super();
+      }
+
+      connectedCallback() {
+        this.render();
+        this.addListeners();
+      }
+
+      render() {
+        const shadow = this.shadow;
+        shadow.innerHTML = `<div class="contenedor"></div>`;
+        const style = document.createElement("style");
+        style.innerHTML = `
+            * {
+               box-sizing: border-box;
+               margin: 0; 
+            }
+            .contenedor {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+            }
+            .contenedor-carta {
+                display: flex;
+                flex-direction: column;
+            }
+            .foto-mascota {
+                height: 180px;
+                max-width: 350px;
+            }
+        `;
+        shadow.appendChild(style);
+      }
+
+      addListeners() {
+        cerquita().then((result) => {
+          this.shadow.querySelector(".contenedor").innerHTML = `${petsHtml(
+            result
+          )}`;
+        });
+        //
+      }
+    }
+  );
+};
+
+const cerquita = async () => {
+  const token = `Bearer ${state.getUserData().token}`;
+  return await api.mascotasReportedByUser(token);
+};
+
+const petsHtml = (pets) => {
+  return pets
+    .map((pet) => {
+      return /*html*/ `<pet-card-el 
+      idPet="${pet.id.toString()}"
+      name="${pet.name.toString()}"
+      description="${pet.description.toString()}"
+      pictureURL="${pet.pictureURL.toString()}"
+      last_location="${pet.last_location.toString()}"
+      founded="${pet.founded.toString()}"
+      remove="yes"></pet-card-el>`;
+    })
+    .join("");
+};
