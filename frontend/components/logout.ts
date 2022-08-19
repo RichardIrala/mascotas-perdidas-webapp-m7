e2bb4f77be1b8e6eb7b6dda2a2ae437c606728d9
RichardIrala <richardirala47@hotmail.com>
2022-08-19 12:33:53 -0300

    Se creo que componente logout para cerrar sesión

---
+++ frontend/components/logout.ts
@@ -0,0 +1,40 @@
+import { state } from "../state";
+
+export const instanciar_logout = () => {
+  customElements.define(
+    "logout-el",
+    class extends HTMLElement {
+      shadow = this.attachShadow({ mode: "open" });
+      constructor() {
+        super();
+      }
+      connectedCallback() {
+        this.render();
+      }
+      render() {
+        const style = document.createElement("style");
+        this.shadow.innerHTML = /* html */ `
+        <button class="logout" type="button">
+            <button-rose-el>Cerrar sesión</button-rose-el>
+        </button>`;
+
+        style.innerHTML = `
+        .logout {
+            border: none;
+            padding: 0;
+        }
+        `;
+        this.shadow.appendChild(style);
+        this.addListeners();
+      }
+      addListeners() {
+        const $ = (selector: `.${any}`) => this.shadow.querySelector(selector);
+        const logoutButton = $(".logout");
+        logoutButton.addEventListener("click", () => {
+          state.clearUserData();
+          state.checkUserToken();
+        });
+      }
+    }
+  );
+};
