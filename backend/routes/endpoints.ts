54a813339edfd3ae31ea7f765b420151472a8ed7
RichardIrala <richardirala47@hotmail.com>
2022-08-22 21:55:09 -0300

    Se movio un endpoint de dev a penultimo

--- backend/routes/endpoints.ts
+++ backend/routes/endpoints.ts
@@ -164,13 +164,13 @@ app.post("/pets/petinfo/email", authMiddleware, async (req, res) => {
   }
 });
 
-//todo lo que no coincida con una ruta, devuelve el index.html
-app.get("*", (req, res) => {
-  res.sendFile(frontend + "/index.html");
-});
-
 //Reinicia la BD - util para usar en el deploy
 app.get("/sync-force", async (req, res) => {
   await sequelize.sync({ force: true });
   res.json({ message: "sync completo" });
+});
+
+//todo lo que no coincida con una ruta, devuelve el index.html
+app.get("*", (req, res) => {
+  res.sendFile(frontend + "/index.html");
 });
