f8886893e7a7223d1165c7a385a9f788ac424dc4
RichardIrala <richardirala47@hotmail.com>
2022-09-02 12:58:19 -0300

    Se tipo el controlador userExist de controllers/User

--- backend/controllers/User.ts
+++ backend/controllers/User.ts
@@ -1,8 +1,9 @@
 import { User } from "../models";
 
 //Retorna un booleano luego de chequear si X usuario existe en nuestra base de datos
-export const userExist = async (email: string) => {
+export const userExist = async (email: string): Promise<{ exist: boolean }> => {
   const user = await User.findOne({ where: { email } });
   const exist = user ? true : false;
+
   return { exist };
 };
